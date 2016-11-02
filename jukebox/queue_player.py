622450c5cc916cb213f4b8d0fa6fed2d087f9c6f
Jan-Pascal van Best <janpascal@vanbest.org>
2016-11-02 16:33:42 +0100

    Use virtual path as filename in icecast stream

--- jukebox/queue_player.py
+++ jukebox/queue_player.py
@@ -53,7 +53,7 @@ def _play_thread():
             song.now_playing = True
             song.save()
 
-            player.play(song.mp3_path())
+            player.play(song.mp3_path(), song.tree_path())
             while not player.stopped_event.wait(0.1):
                 del song.skipped
                 if song.skipped or skip_rest_of_current_folder or stop_playing:
